7bc00a8ada2043b4ccc17e295f3d0745077d38ad
meijiawen <meijiawen@pjlab.org.cn>
2023-07-06 18:45:58 +0800

    device

--- app.py
+++ app.py
@@ -2,11 +2,14 @@
 from transformers import AutoTokenizer, AutoModel
 import gradio as gr
 
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 tokenizer = AutoTokenizer.from_pretrained("internlm/internlm-chat-7b",
                                           trust_remote_code=True)
+# model = AutoModel.from_pretrained("internlm/internlm-chat-7b",
+#                                   trust_remote_code=True,
+#                                   device='cuda')
 model = AutoModel.from_pretrained("internlm/internlm-chat-7b",
-                                  trust_remote_code=True,
-                                  device='cuda')
+                                  trust_remote_code=True)
 
 
 def generate(prompt):
