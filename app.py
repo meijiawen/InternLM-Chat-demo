c6f9940f7d01e791436490516da5aebd750fc51c
meijiawen <meijiawen@pjlab.org.cn>
2023-07-07 10:59:10 +0800

    internlm

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 # from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-from transformers import AutoTokenizer, AutoModel
+from transformers import AutoTokenizer, AutoModelForCausalLM
 import gradio as gr
 
 # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # tokenizer = AutoTokenizer.from_pretrained("internlm/internlm-chat-7b",
 #                                           trust_remote_code=True)
-tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b",
+
+tokenizer = AutoTokenizer.from_pretrained("internlm/internlm-chat-7b",
                                           trust_remote_code=True)
-# model = AutoModel.from_pretrained("internlm/internlm-chat-7b",
-#                                   trust_remote_code=True,
-#                                   device='cuda')
-# model = AutoModel.from_pretrained("internlm/internlm-chat-7b",
-#                                   trust_remote_code=True)
-model = AutoModel.from_pretrained("THUDM/chatglm-6b",
-                                  trust_remote_code=True).half().cuda()
+model = AutoModelForCausalLM.from_pretrained("internlm/internlm-chat-7b",
+                                             trust_remote_code=True).cuda()
+
+# tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b",
+#                                           trust_remote_code=True)
+# model = AutoModel.from_pretrained("THUDM/chatglm-6b",
+#                                   trust_remote_code=True).half().cuda()
 
 
 def predict(input, history=None):
@@ -40,13 +41,13 @@ with gr.Blocks() as demo:
     button.click(predict, [txt, state], [chatbot, state])
 demo.queue().launch()
 
-input_component = gr.Textbox(label="Prompt")
-output_component = gr.Textbox(label="output")
-examples = [["今天天气如何？"], ["你是谁？"]]
-description = "浦语在线体验1.0"
-gr.Interface(generate,
-             inputs=input_component,
-             outputs=output_component,
-             examples=examples,
-             title="InternLM Chat demo v1.0",
-             description=description).launch()
+# input_component = gr.Textbox(label="Prompt")
+# output_component = gr.Textbox(label="output")
+# examples = [["今天天气如何？"], ["你是谁？"]]
+# description = "浦语在线体验1.0"
+# gr.Interface(generate,
+#              inputs=input_component,
+#              outputs=output_component,
+#              examples=examples,
+#              title="InternLM Chat demo v1.0",
+#              description=description).launch()
